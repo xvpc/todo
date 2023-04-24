d8550d9939938fe049173ee980fd0b9ecf7dc24b
Viper <viperxx100@gmail.com>
2023-04-24 08:36:38 +0300

    #

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '@/styles/style.css'
-import '../styles/scss/style.scss'
 
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
